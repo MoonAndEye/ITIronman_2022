5b2dafb29f36a9b29ddbb53c0c773eaaa02f8135
MarvinLin <atimis19@gmail.com>
2022-08-28 21:57:25 +0800

    add: SwiftUI 基本元件

--- SwiftUITutorial/SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/SwiftUITutorial/ContentView.swift
@@ -2,20 +2,56 @@
 //  ContentView.swift
 //  SwiftUITutorial
 //
-//  Created by cm0679 on 2022/8/28.
+//  Created by Marvin on 2022/8/28.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  
+  @State var inputString = ""
+  let colors: [Color] =
+      [.red, .orange, .yellow, .green, .blue, .purple]
+
+  var body: some View {
+      ZStack {
+          ForEach(0..<colors.count) {
+              Rectangle()
+                  .fill(colors[$0])
+                  .frame(width: 100, height: 100)
+                  .offset(x: CGFloat($0) * 10.0,
+                          y: CGFloat($0) * 10.0)
+          }
+      }
+  }
+  
+  
+//  var body: some View {
+    
+//    HStack {
+//
+//      Text(inputString)
+//        .bold()
+//        .padding()
+//      TextField("input some text", text: $inputString)
+//        .padding()
+//    }
+    
+//    Button {
+//      print("a button was tapped")
+//    } label: {
+//      Image(systemName: "airplane.departure")
+//        .font(.system(size: 50))
+//        .foregroundColor(.red)
+//        .background {
+//          Color.gray
+//        }
+//    }
+//  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
