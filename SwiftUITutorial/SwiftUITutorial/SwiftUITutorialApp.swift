017f48bcada1f367ee55b9bf26fba5d8b1076e51
MarvinLin <atimis19@gmail.com>
2022-08-28 11:43:43 +0800

    Project init, tutorial

---
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by cm0679 on 2022/8/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
