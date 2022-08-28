d143dd9a7a8b17f23f0ec61cb078df59b70c32c9
MarvinLin <atimis19@gmail.com>
2022-08-28 22:29:28 +0800

    project init: Apple doc manage ui state

---
+++ ManageUIState/ManageUIState/ManageUIStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManageUIStateApp.swift
+//  ManageUIState
+//
+//  Created by cm0679 on 2022/8/28.
+//
+
+import SwiftUI
+
+@main
+struct ManageUIStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
