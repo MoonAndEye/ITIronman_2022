ba4048a8048ee940f0affa9a5a1c930ac8a161f0
MarvinLin <atimis19@gmail.com>
2022-08-28 22:39:37 +0800

    update: Apple 文件中的範例，Episode 和 ui state

--- ManageUIState/ManageUIState/ContentView.swift
+++ ManageUIState/ManageUIState/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PlayButton: View {
+    @Binding var isPlaying: Bool
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            withAnimation(.easeInOut(duration: 1)) {
+                self.isPlaying.toggle()
+            }
+        }) {
+            Image(systemName: isPlaying ? "pause.circle" : "play.circle")
+                .scaleEffect(isPlaying ? 1 : 1.5)
+        }
+    }
+}
+
+struct PlayerView: View {
+    var episode: Episode
+    @State private var isPlaying: Bool = false
+    
+    var body: some View {
+        VStack {
+            Text(episode.title)
+            Text(episode.showTitle)
+            PlayButton(isPlaying: $isPlaying)
+        }
+        .background(isPlaying ? Color.green : Color.red) // Transitions with animation.
+    }
+}
+
+struct ContentView: View {
+    
+    @State var episode = Episode(title: "Some Episode", showTitle: "Great show", isFavorite: false)
+    
+    var body: some View {
+        PlayerView(episode: episode)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
+    static var episode: Episode = Episode(title: "Some Episode", showTitle: "Great show", isFavorite: false)
+    
     static var previews: some View {
         ContentView()
+        PlayerView(episode: episode)
     }
 }
---
+++ ManageUIState/ManageUIState/Episode.swift
@@ -0,0 +1,16 @@
+//
+//  Episode.swift
+//  ManageUIState
+//
+//  Created by cm0679 on 2022/8/28.
+//
+
+import Foundation
+
+struct Episode {
+    
+    var title: String
+    var showTitle: String
+    var isFavorite: Bool
+}
+
