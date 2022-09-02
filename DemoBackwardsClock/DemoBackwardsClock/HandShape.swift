982752a8052c0d04fbebde58e5e3012c3c8b4850
MarvinLin <marvin_lin@cmoney.com.tw>
2022-09-02 17:43:57 +0800

    update: 完成 時分秒針的宣告

--- DemoBackwardsClock/DemoBackwardsClock/HandShape.swift
+++ DemoBackwardsClock/DemoBackwardsClock/HandShape.swift
@@ -7,58 +7,77 @@
 
 import SwiftUI
 
-//struct HandShape: Shape {
-//
-//    var spacing: CGFloat = 2
-//    var spacing: CGFloat {
-//        return handLength.getSpacing()
-//    }
-//
-//    var handLength: HandLength
-//
-//    func path(in rect: CGRect) -> Path {
-//
-//        var p = Path()
-//
-//        p.move(to: CGPoint(x: rect.midX - spacing, y: rect.midY))
-//        p.addLine(to: CGPoint(x: rect.midX, y: getHandPointY(rect: rect, handLength: handLength)))
-//        p.addLine(to: CGPoint(x: rect.midX + spacing, y: rect.midY))
-//
-//        return p
-//    }
-//
-//    func getHandPointY(rect: CGRect, handLength: HandLength) -> CGFloat {
-//
-//        let edge = min(rect.size.width, rect.size.height)
-//        return edge * ( 1 - handLength.getLengthRatio())
-//    }
-//}
+enum HandLength {
+
+    case second
+    case minute
+    case hour
+    
+    func getLengthRatio() -> Double {
+        switch self {
+        case .second:
+            return 1
+        case .minute:
+            return 0.9
+        case .hour:
+            return 0.7
+        }
+    }
+    
+    func getSpacing() -> CGFloat {
+        switch self {
+        case .second:
+            return 2
+        case .minute:
+            return 4
+        case .hour:
+            return 6
+        }
+    }
+}
 
 struct HandShape: Shape {
     
-    var spacing: CGFloat = 2
+    var spacing: CGFloat {
+        return handLength.getSpacing()
+    }
+    
+    var handLength: HandLength
     
     func path(in rect: CGRect) -> Path {
         
         var p = Path()
 
         p.move(to: CGPoint(x: rect.midX - spacing, y: rect.midY))
-        p.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        p.addLine(to: CGPoint(x: rect.midX, y: getHandPointY(rect: rect, handLength: handLength)))
         p.addLine(to: CGPoint(x: rect.midX + spacing, y: rect.midY))
         
         return p
     }
+    
+    func getHandPointY(rect: CGRect, handLength: HandLength) -> CGFloat {
+        
+        let edge = min(rect.size.width, rect.size.height)
+        return edge * ( 1 - handLength.getLengthRatio())
+    }
 }
-
 struct HandShape_Previews: PreviewProvider {
     static var previews: some View {
-        HandShape()
-            .previewLayout(.fixed(width: 100, height: 100))
-        HandShape()
-            .fill(Color.blue)
-            .previewLayout(.fixed(width: 100, height: 100))
-        HandShape()
-            .fill(Color.init(uiColor: .systemCyan))
-            .previewLayout(.fixed(width: 100, height: 100))
+        
+        Group {
+            HandShape(handLength: .second)
+                .fill(Color.blue)
+                .frame(width: 200, height: 200, alignment: .center)
+                .background(.cyan)
+            HandShape(handLength: .minute)
+                .fill(Color.red)
+                .frame(width: 200, height: 200, alignment: .center)
+                .background(.cyan)
+            HandShape(handLength: .hour)
+                .fill(Color.brown)
+                .frame(width: 200, height: 200, alignment: .center)
+                .background(.cyan)
+        }
+        .previewLayout(.sizeThatFits)
     }
 }
