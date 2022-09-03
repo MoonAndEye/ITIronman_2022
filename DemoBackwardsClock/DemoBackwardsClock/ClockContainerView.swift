8a95997285f2ec68bd70506ee80840eef60b7db5
MarvinLin <marvin_lin@cmoney.com.tw>
2022-09-03 16:20:44 +0800

    feat: 組合錶盤和時分秒針

--- DemoBackwardsClock/DemoBackwardsClock/ClockContainerView.swift
+++ DemoBackwardsClock/DemoBackwardsClock/ClockContainerView.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct ClockContainerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  
+  var width: CGFloat = 200
+  var height: CGFloat = 200
+  
+  var body: some View {
+    ZStack {
+      ClockDialView()
+      HandShape(handLength: .hour)
+        .fill(Color.blue)
+        .rotationEffect(Angle(degrees: 300))
+      HandShape(handLength: .minute)
+        .fill(Color.cyan)
+        .rotationEffect(Angle(degrees: 60))
+      HandShape(handLength: .second)
+        .fill(Color.red)
+        .rotationEffect(Angle(degrees: 180))
     }
+    .frame(width: width, height: height, alignment: .center)
+  }
 }
 
 struct ClockContainerView_Previews: PreviewProvider {
-    static var previews: some View {
-        ClockContainerView()
-    }
+  static var previews: some View {
+    ClockContainerView()
+  }
 }
