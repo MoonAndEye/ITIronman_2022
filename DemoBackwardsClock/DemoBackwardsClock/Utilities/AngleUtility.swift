a71354b01966ad1a01f01ae784f3837688763fa1
MarvinLin <marvin_lin@cmoney.com.tw>
2022-09-04 23:04:42 +0800

    mov: 開出 Views Group 和 AngleUtility

---
+++ DemoBackwardsClock/DemoBackwardsClock/Utilities/AngleUtility.swift
@@ -0,0 +1,8 @@
+//
+//  AngleUtility.swift
+//  DemoBackwardsClock
+//
+//  Created by cm0679 on 2022/9/4.
+//
+
+import Foundation
--- DemoBackwardsClock/DemoBackwardsClock/Views/ClockContainerView.swift
+++ DemoBackwardsClock/DemoBackwardsClock/Views/ClockContainerView.swift
@@ -12,20 +12,34 @@ struct ClockContainerView: View {
   var width: CGFloat = 200
   var height: CGFloat = 200
   
+  @State var hourDegree: Double = 0
+  @State var minuteDegree: Double = 0
+  @State var secondDegree: Double = 0
+  
+  private let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+  
   var body: some View {
     ZStack {
       ClockDialView()
       HandShape(handLength: .hour)
         .fill(Color.blue)
-        .rotationEffect(Angle(degrees: 300))
+        .rotationEffect(Angle(degrees: hourDegree))
       HandShape(handLength: .minute)
         .fill(Color.cyan)
-        .rotationEffect(Angle(degrees: 60))
+        .rotationEffect(Angle(degrees: minuteDegree))
       HandShape(handLength: .second)
         .fill(Color.red)
-        .rotationEffect(Angle(degrees: 180))
+        .rotationEffect(Angle(degrees: secondDegree))
     }
     .frame(width: width, height: height, alignment: .center)
+    .onReceive(timer) { _ in
+      updateTime()
+    }
+  }
+  
+  private func updateTime() {
+    let timestamp = Date().timeIntervalSince1970
+    print("current timestamp: \(timestamp)")
   }
 }
 
